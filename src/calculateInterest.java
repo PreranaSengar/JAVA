94b9511025a7ca4c591a47bb66134a03c9243230
Prerana sengar <preranasengar@gmail.com>
2024-12-15 19:02:42 +0530

    calculate simple and compound interest

---
+++ src/calculateInterest.java
@@ -0,0 +1,17 @@
+public class calculateIntreset {
+
+        public static void main(String []args){
+            double p, r, t, s_interest, c_interest;
+            p = 50000;
+            r = 20;
+            t = 5;
+            System.out.println("Principal = "+p);
+            System. out. println("Annual Rate of Interest = "+r);
+            System. out. println("Time (years) = "+t);
+            s_interest = (p * r * t)/100;
+            c_interest = p * Math.pow(1.0+r/100.0,t) - p;
+            System.out.println("Simple Interest: "+s_interest);
+            System.out. println("Compound Interest: "+c_interest);
+        }
+    }
+
